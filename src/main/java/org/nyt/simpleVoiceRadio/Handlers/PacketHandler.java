27fce285ae1caa41e4a2a618e594c5b04921f63d
Nybik_YT <d.nasadyuk123@gmail.com>
2026-01-16 16:04:02 +0200

    Test

--- src/main/java/org/nyt/simpleVoiceRadio/Handlers/PacketHandler.java
+++ src/main/java/org/nyt/simpleVoiceRadio/Handlers/PacketHandler.java
@@ -29,7 +29,7 @@ public class PacketHandler {
                 if (event.getPacket().getIntegers().read(0).equals(1010)) {
                     Jukebox jukebox = (Jukebox) event.getPacket().getBlockPositionModifier().read(0).toLocation(event.getPlayer().getWorld()).getBlock().getState();
                     if (jukebox.getRecord().getItemMeta().getJukeboxPlayable().getSong().getKey().getNamespace().equals("simple_voice_radio")) {
-                        event.getPlayer().sendMessage("not true");
+                        event.getPlayer().sendMessage("true");
                         event.setCancelled(true);
                     }
                 }
