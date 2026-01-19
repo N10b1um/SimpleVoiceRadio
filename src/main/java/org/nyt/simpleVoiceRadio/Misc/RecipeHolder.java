c7bb65462bbbf28226be25bb73bafad98a374882
Nybik_YT <d.nasadyuk123@gmail.com>
2026-01-19 11:40:21 +0200

    added view_craft command

---
+++ src/main/java/org/nyt/simpleVoiceRadio/Misc/RecipeHolder.java
@@ -0,0 +1,18 @@
+package org.nyt.simpleVoiceRadio.Misc;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.jetbrains.annotations.NotNull;
+
+public class RecipeHolder implements InventoryHolder {
+    private Inventory inventory;
+
+    @Override
+    public @NotNull Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inv) {
+        this.inventory = inv;
+    }
+}
